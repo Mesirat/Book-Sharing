319a2699c666521c479912d2f66c172fda486ead
Mesirat <mesiratbelete216@gmail.com>
2024-12-20 18:14:06 -0800

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/tailwind.css'
+import Login from './pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <p>Hello</p>
-    </>
+   <Login/>
   )
 }
 
